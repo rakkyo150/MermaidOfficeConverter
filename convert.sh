1b74d28e5e4979e977ba87fdc80f9825fdd78c20
rakkyo150 <86054813+rakkyo150@users.noreply.github.com>
2024-09-27 17:42:13 +0900

    バグ修正とより使いやすくした

--- convert.sh
+++ convert.sh
@@ -8,5 +8,15 @@ mmdc -f -i "$input_file" -o "$filename.pdf"
 pdf2svg "$filename-1.pdf" "$filename.svg"
 rm "$filename-1.pdf"
 echo "変換が終了しました: $filename.svg"
-open .
-pause
+
+if [ -f /proc/version ] && grep -q Microsoft /proc/version; then
+    # WSLの場合
+    cmd.exe /c explorer .
+elif [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]]; then
+    explorer .
+else
+# Windowsなら`explorer .`
+  open .
+fi
+
+# read
